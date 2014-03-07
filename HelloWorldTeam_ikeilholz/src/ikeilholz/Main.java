97cc657ce63bc03eed397316ce7286e52c15da15
Brinnich Selina <sbrinnich@student.tgm.ac.at>
2014-03-07 11:17:02 +0100

    2. Methode hinzugefügt

--- HelloWorldTeam_ikeilholz/src/ikeilholz/Main.java
+++ HelloWorldTeam_ikeilholz/src/ikeilholz/Main.java
@@ -5,11 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		inga();
+		selina();
 
 	}
 	
 	public static void inga(){
 		System.out.println("Schöne Grüße von Inga!");
+	}
+	
+	public static void selina(){
+		System.out.println("Schöne Grüße von Selina!");
 	}
 
 }
